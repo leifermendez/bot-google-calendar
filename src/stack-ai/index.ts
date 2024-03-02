c47511686bee13bf45efee2cee6c7b4b152f4fac
Leifer Mendez <leifer33@gmail.com>
2024-03-02 16:16:38 +0100

    config

--- src/stack-ai/index.ts
+++ src/stack-ai/index.ts
@@ -1,10 +1,12 @@
+import { STACK_AI_API, STACK_AI_URL } from "src/config";
+
 export async function query(data: { "in-0": string }) {
     const response = await fetch(
-        "https://www.stack-inference.com/run_deployed_flow?flow_id=65e0dccda5962775219ba742&org=501a4204-4f0a-4a5e-b5bc-d866cbf13fae",
+        STACK_AI_URL,
         {
             headers: {
                 'Authorization':
-                    'Bearer 01957069-09ac-482b-8d9a-d3526a76a163',
+                    `Bearer ${STACK_AI_API}`,
                 'Content-Type': 'application/json'
             },
             method: "POST",
